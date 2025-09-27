984d2452a055632c092019d677eca99a6515f2c0
Chauhan Hitarth <chauhanhitarth6@gmail.com>
2025-09-27 13:11:06 +0000

    Try fixing this error: `Console Error: Error: [GoogleGenerativeAI Error]

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,11 @@
 import {genkit} from 'genkit';
-import {googleAI, gemini15Flash} from '@genkit-ai/googleai';
+import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
-  model: gemini15Flash,
+  plugins: [
+    googleAI({
+      apiVersion: 'v1',
+    }),
+  ],
+  model: 'googleai/gemini-pro',
 });
