7d88f18315016c902c0d9c6054dfb941bc7ed00f
Chauhan Hitarth <chauhanhitarth6@gmail.com>
2026-02-06 10:27:08 +0000

    Add one more admin page to show manage subscription of users, their orig

---
+++ src/app/admin/subscriptions/page.tsx
@@ -0,0 +1,5 @@
+import { SubscriptionsManagementPage } from "@/components/admin/subscriptions-management";
+
+export default function AdminSubscriptionsPage() {
+    return <SubscriptionsManagementPage />;
+}
