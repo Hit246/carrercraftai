2d650ec4801416432652439fc1141560be18ec0d
Chauhan Hitarth <chauhanhitarth6@gmail.com>
2026-06-03 05:38:33 +0000

    Patch : Removed un neccessary auth modal pop up

--- src/components/auth-modal.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-/**
- * @deprecated This component is no longer used. Authentication is handled via 
- * dedicated /login and /signup pages.
- */
-export function AuthModal() {
-  return null;
-}
