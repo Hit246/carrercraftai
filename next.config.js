ae890a8dda0c47ae13355ed2f71ab9200c793e1b
Chauhan Hitarth <chauhanhitarth6@gmail.com>
2026-06-12 13:18:52 +0000

    Try fixing this error: `Console Error: Body exceeded 1 MB limit. To conf

--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const withPWA = require('next-pwa')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverActions: {
+    bodySizeLimit: '10mb',
+  },
   async redirects() {
     return [
       {
